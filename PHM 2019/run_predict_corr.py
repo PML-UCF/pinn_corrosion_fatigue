79621a5e47d3c555671292f8eb7a85a527f6b6bc
Arinan Dourado <arinandourado@knights.ucf.edu>
2019-07-28 03:35:15 -0400

    Rnn prediction file created and updated.

--- PHM 2019/run_predict_corr.py
+++ PHM 2019/run_predict_corr.py
@@ -42,15 +42,12 @@
 # SOFTWARE.
 # ==============================================================================
 
-""" Train physics-informed recursive neural network
+""" Prediction physics-informed recursive neural network
 """
-
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 import time
-
-import os
 
 import matplotlib.pyplot as plt
 
@@ -58,21 +55,19 @@ from tensorflow.python.framework import ops
 
 from corr_model import create_model
 
-np.random.seed(31)
-
 if __name__ == "__main__":
     start = time.time()
     #--------------------------------------------------------------------------
     # pre- processing
     myDtype = tf.float32  # defining type for the layer
     
-    dfcidx = pd.read_csv('rnn_training_idx.csv', index_col = None) # loading corrosion index data
+    dfcidx = pd.read_csv('rnn_predict_idx.csv', index_col = None) # loading corrosion index data
     cidx = dfcidx.values[:,1:-1]
-    dfdS = pd.read_csv('rnn_training_dS.csv', index_col = None) # loading mech. load data    
+    dfdS = pd.read_csv('rnn_predict_dS.csv', index_col = None) # loading mech. load data    
     dS = dfdS.values[:,1:-1]
-    dfR = pd.read_csv('rnn_training_R.csv', index_col = None) # loading stress ratio data
-    R = dfR.values[:,1:-1]    
-    
+    dfR = pd.read_csv('rnn_predict_R.csv', index_col = None) # loading stress ratio data
+    R = dfR.values[:,1:-1]
+            
     nFleet, nCycles  = np.shape(cidx) 
     
     # RNN inputs
@@ -80,83 +75,62 @@ if __name__ == "__main__":
     input_array = np.dstack((input_array, cidx))
     inputTensor = ops.convert_to_tensor(input_array, dtype = myDtype)
     
-    a0 = pd.read_csv('rnn_training_a0.csv', index_col = None)
+    a0 = pd.read_csv('rnn_predict_a0.csv', index_col = None)
     
     a0RNN = np.zeros(input_array.shape[0]) 
     a0RNN[:] = a0.values[:,-1] # initial crack length
     a0RNN = np.reshape(a0RNN,(len(a0RNN),1))
     a0RNN = ops.convert_to_tensor(a0RNN, dtype=myDtype)
-    
-    ainsp = pd.read_csv('rnn_training_a_insp.csv', index_col = None)
-    
-    aTarget = ainsp.values[:,-1] # inpection crack data
-    aTarget = aTarget[:, np.newaxis]
-    aTarget = ops.convert_to_tensor(aTarget, dtype=myDtype) 
     #--------------------------------------------------------------------------
     batch_input_shape = input_array.shape
     #--------------------------------------------------------------------------
-    # Loading MLP info and parameters    
-    MLP_C_layer = tf.keras.models.load_model('log_C_MLP.h5')
-    MLP_C_layer.trainable = True
-    
-    MLP_m_layer = tf.keras.models.load_model('m_MLP.h5')
-    MLP_m_layer.trainable = True
-    
+    # loading MLP scaling bounds 
     df = pd.read_csv('MLP_training_data.csv', index_col = None) 
     
     low_C = np.asarray(np.log(df[['C']]).min(axis=0))
     up_C = np.asarray(np.log(df[['C']]).max(axis=0))    
     
     low_m = np.asarray(df[['m']].min(axis=0))
-    up_m = np.asarray(df[['m']].max(axis=0)) 
-            
+    up_m = np.asarray(df[['m']].max(axis=0))
+    
     selectaux = [2,3] # for input selection on the MLPS
     selectdk = [0,1] # for input selection on the StressIntensity layer
     
     F = 2.8   # geometry factor for the stress intensity layer  
+    #--------------------------------------------------------------------------    
+    # Loading plane MLP     
+    MLP_C_p = tf.keras.models.load_model('log_C_MLP.h5')
+    MLP_C_p.trainable = False
     
-    model = create_model(MLP_C_layer, MLP_m_layer, low_C, up_C, low_m, up_m, F, a0RNN, 
-                         batch_input_shape, selectaux, selectdk, myDtype)
-   #--------------------------------------------------------------------------
-    EPOCHS = 250
+    MLP_m_p = tf.keras.models.load_model('m_MLP.h5')
+    MLP_m_p.trainable = False
+    
+    model = create_model(MLP_C_p, MLP_m_p, low_C, up_C, low_m, up_m, F, a0RNN, 
+                         batch_input_shape, selectaux, selectdk, myDtype, return_sequences = True)
+    before = model.predict_on_batch(input_array)    
     
     checkpoint_path = "training_20_pts/cp.ckpt"
-    checkpoint_dir = os.path.dirname(checkpoint_path)
-
-    # Create checkpoint callback
-    cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
-                                                 save_weights_only=True,
-                                                 verbose=1)
-    
-    history = model.fit(inputTensor, aTarget, steps_per_epoch=1, epochs=EPOCHS, callbacks = [cp_callback])
-    
-    training_loss = history.history['loss']
+    model.load_weights(checkpoint_path)
+    after = model.predict_on_batch(input_array)  
     #--------------------------------------------------------------------------
-    outputs = ainsp.values[:,-1]
-    results = model.predict_on_batch(input_array)
-    # Create count of the number of epochs
-    epoch_count = range(1, len(training_loss) + 1)
-    # Visualize loss history
-    fig  = plt.figure(1)
-    fig.clf()
-  
-    plt.plot(epoch_count, training_loss, 'r--')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.title('Loss function (MLP)')
-    plt.show();
-    #--------------------------------------------------------------------------    
-    # Plot actual x predict 
-    fig  = plt.figure(2)
-    fig.clf()
+    amech = pd.read_csv('rnn_predict_a_mech.csv', index_col = None)
+    amech = amech.values[:,-1]
     
-    plt.plot(outputs*1e3,outputs*1e3,'--k')
-    plt.plot(outputs*1e3,results*1e3,'ob')
+    cr = pd.read_csv('rnn_predict_a_value.csv', index_col = None)
+    cr = cr.values[:,-1]
     
-    plt.title('Crack length [mm]')
-    plt.xlabel('Actual')
-    plt.ylabel('Predicted')
+    idx = np.linspace(0,98,99,dtype = int) 
+    idx = np.delete(idx, [20])    # filtering a corrupt datapoint
+    
+    fig  = plt.figure()
+    fig.clf()
+    plt.plot(after[idx,-1,0]*1e3,'xk', label = 'rnn prediction')
+    plt.plot(amech[idx]*1e3,'sb', label = 'fatigue')
+    plt.plot(cr[idx]*1e3,'om', label = 'corrosion-fatigue')
+    
+    plt.ylabel('a [mm]')
     plt.grid(which = 'both')
+    plt.legend(loc=0, facecolor = 'w')
     
     print('Elapsed time is %s seconds'%(time.time()-start))
     
